a2c162f643b8bbd809d1bbf54990b71d3ad3e536
chrism <chrism@kingorange.com>
2021-04-15 15:14:45 -0700

    questions 1 and 2

---
+++ question_2.py
@@ -0,0 +1,21 @@
+"""
+pseudocode
+
+multiply 100 and 10
+store value to variable for later
+
+create total variable with 0 as initial value
+create index variable with 0 as initial value
+
+add 1 and multiply 1 and 2
+add 1 to index
+
+add the product of 1*2 to the initial variable
+multiply the product by 2
+add 1 to index
+
+repeat until index = 10
+
+compare the value of the first stored value to the second and output the larger value
+
+"""
